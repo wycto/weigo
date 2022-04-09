dc268d7d795f6426f2a8338642e4456d7826e482
weiyi <lecent2020>
2022-04-09 22:50:38 +0800

    控制器模版使用

--- controller.go
+++ controller.go
@@ -8,59 +8,61 @@ import (
 	"html/template"
 	"io"
 	"net/http"
+	"os"
 	"strings"
+	"unsafe"
 )
 
-//控制器类
+// Controller 控制器类
 type Controller struct {
 	Context *Context
 	data    map[string]interface{}
 }
 
-//控制器初始化
+// Init 控制器初始化
 func (controller *Controller) Init(context *Context) {
 	controller.Context = context
 	controller.data = make(map[string]interface{})
 }
 
-//控制器默认的请求方法
+// Index 控制器默认的请求方法
 func (controller *Controller) Index() {
 	http.NotFound(controller.Context.ResponseWriter, controller.Context.Request)
 }
 
-//列表方法
+// List 列表方法
 func (controller *Controller) List() {
 	http.NotFound(controller.Context.ResponseWriter, controller.Context.Request)
 }
 
-//详情方法
+// View 详情方法
 func (controller *Controller) View() {
 	http.NotFound(controller.Context.ResponseWriter, controller.Context.Request)
 }
 
-//删除方法
+// Delete 删除方法
 func (controller *Controller) Delete() {
 	http.NotFound(controller.Context.ResponseWriter, controller.Context.Request)
 }
 
-//修改方法
+// Update 修改方法
 func (controller *Controller) Update() {
 	http.NotFound(controller.Context.ResponseWriter, controller.Context.Request)
 }
 
-//页面模版赋值
+// Assign 页面模版赋值
 func (controller *Controller) Assign(Key string, Value interface{}) {
 	controller.data[Key] = Value
 }
 
-//页面模版渲染
+// Display 页面模版渲染
 func (controller *Controller) Display(viewName string) {
 	if viewName == "" {
-		if Config.App.MultiApp == true {
-			viewName = RootPath + DS + Config.View.RootPath + DS + strings.ToLower(controller.Context.AppName) + DS + strings.ToLower(controller.Context.ControllerName) + DS + strings.ToLower(controller.Context.ActionName) + ".html"
-		} else {
-			viewName = RootPath + DS + Config.View.RootPath + DS + strings.ToLower(controller.Context.ControllerName) + DS + strings.ToLower(controller.Context.ActionName) + ".html"
+		viewName = RootPath + DS + Config.View.RootPath
+		if controller.Context.AppName != "" {
+			viewName = viewName + DS + controller.Context.AppName
 		}
+		viewName = viewName + DS + strings.ToLower(controller.Context.ControllerName) + DS + strings.ToLower(controller.Context.ActionName) + ".html"
 	}
 
 	t, err := template.ParseFiles(viewName)
@@ -71,22 +73,40 @@ func (controller *Controller) Display(viewName string) {
 	}
 }
 
-//输出成功的json格式
+// ShowHtml 页面模版渲染
+func (controller *Controller) ShowHtml(viewName string) {
+	if viewName == "" {
+		viewName = RootPath + DS + Config.View.RootPath
+		if controller.Context.AppName != "" {
+			viewName = viewName + DS + controller.Context.AppName
+		}
+		viewName = viewName + DS + strings.ToLower(controller.Context.ControllerName) + DS + strings.ToLower(controller.Context.ActionName) + ".html"
+	}
+
+	f, err := os.ReadFile(viewName)
+	if err != nil {
+		io.WriteString(controller.Context.ResponseWriter, err.Error())
+	} else {
+		io.WriteString(controller.Context.ResponseWriter, *(*string)(unsafe.Pointer(&f)))
+	}
+}
+
+// ResponseSuccess 输出成功的json格式
 func (controller *Controller) ResponseSuccess(msg string, data interface{}) {
 	controller.Context.ResponseSuccess(msg, data)
 }
 
-//输出失败的json格式
+// ResponseError 输出失败的json格式
 func (controller *Controller) ResponseError(msg string, data interface{}) {
 	controller.Context.ResponseError(msg, data)
 }
 
-//输出系统定义好的错误代码格式json数据
+// ResponseErrorMessage 输出系统定义好的错误代码格式json数据
 func (controller *Controller) ResponseErrorMessage(message *Message, data interface{}) {
 	controller.Context.ResponseMessage(message, data)
 }
 
-//输出字符串
+// ResponseString 输出字符串
 func (controller *Controller) ResponseString(msg string) {
 	controller.Context.ResponseString(msg)
 }
