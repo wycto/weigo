150891decbc8fcb78b1606f0dc5d97ce8a1df245
weiyi <lecent2020>
2022-04-13 21:06:31 +0800

    IP地址获取优化，mac可执行文件

--- functions.go
+++ functions.go
@@ -4,6 +4,7 @@ package weigo
 函数都写在这儿
 */
 import (
+	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -13,6 +14,16 @@ import (
 获取IP地址
 */
 func GetIP() string {
+	ip, err := externalIP()
+	if err != nil {
+		return GetIP2()
+		fmt.Println(err)
+	}
+
+	return ip.String()
+}
+
+func GetIP2() string {
 	var ip = ""
 	address, err := net.InterfaceAddrs()
 
@@ -30,6 +41,52 @@ func GetIP() string {
 			}
 
 		}
+	}
+
+	return ip
+}
+
+func externalIP() (net.IP, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, addr := range addrs {
+			ip := getIpFromAddr(addr)
+			if ip == nil {
+				continue
+			}
+			return ip, nil
+		}
+	}
+	return nil, errors.New("connected to the network?")
+}
+
+func getIpFromAddr(addr net.Addr) net.IP {
+	var ip net.IP
+	switch v := addr.(type) {
+	case *net.IPNet:
+		ip = v.IP
+	case *net.IPAddr:
+		ip = v.IP
+	}
+	if ip == nil || ip.IsLoopback() {
+		return nil
+	}
+	ip = ip.To4()
+	if ip == nil {
+		return nil // not an ipv4 address
 	}
 
 	return ip
